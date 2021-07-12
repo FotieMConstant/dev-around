010b0fed85aff32eff4f560d064757d895833dce
FotieMConstant <fotiemoghommahieconstant@gmail.com>
2021-07-12 18:40:43 +0100

    can now get user location and display thier location on map with markers

--- eru/src/components/MapComponent.vue
+++ eru/src/components/MapComponent.vue
@@ -19,42 +19,70 @@ export default {
       url:
         "https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=" +
         process.env.VUE_APP_API_KEY,
-      zoom: 3,
-      center: [47.41322, -1.219482],
-      bounds: null,
+      geolocation: {
+        longitude: null,
+        latitude: null,
+      },
     };
   },
   mounted() {
-    var map = L.map("map").setView([3.846963, 11.55072], 13);
-
-    L.tileLayer(this.url, {
-      attribution:
-        '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-    }).addTo(map);
-
-    var currentUserIcon = L.icon({
-      iconUrl:
-        "https://www.pngall.com/wp-content/uploads/2017/05/Map-Marker-PNG-File.png",
-      iconSize: [30, 30], // size of the icon
-      popupAnchor: [0, -15], // point from which the popup should open relative to the iconAnchor
-    });
-    L.marker([3.846963, 11.55072], {
-      icon: currentUserIcon,
-    })
-      .addTo(map)
-      .bindPopup("Your location")
-      .openPopup();
+    this.getUserCurrentLocation(); // get user's current location
   },
 
   methods: {
-    zoomUpdated(zoom) {
-      this.zoom = zoom;
+    // zoomUpdated(zoom) {
+    //   this.zoom = zoom;
+    // },
+    // centerUpdated(center) {
+    //   this.center = center;
+    // },
+    // boundsUpdated(bounds) {
+    //   this.bounds = bounds;
+    // },
+    getUserCurrentLocation() {
+      if (!navigator.geolocation) {
+        alert("Geolocation is not supported by your browser");
+      } else {
+        console.log("Locating…");
+        navigator.geolocation.getCurrentPosition(
+          this.successfulGottenLocation,
+          this.errorGettingLocation
+        );
+      }
     },
-    centerUpdated(center) {
-      this.center = center;
+    // when the location is successfully gotten
+    successfulGottenLocation(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      console.log(`Latitude: ${latitude} °, Longitude: ${longitude} °`);
+
+      this.loadMap(latitude, longitude);
     },
-    boundsUpdated(bounds) {
-      this.bounds = bounds;
+    // function to handle when there's an error getting location
+    errorGettingLocation() {
+      alert("Unable to retrieve your location");
+    },
+    loadMap(latitude, longitude) {
+      var map = L.map("map").setView([latitude, longitude], 13);
+
+      L.tileLayer(this.url, {
+        attribution:
+          '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+      }).addTo(map);
+
+      var currentUserIcon = L.icon({
+        iconUrl:
+          "https://www.pngall.com/wp-content/uploads/2017/05/Map-Marker-PNG-File.png",
+        iconSize: [30, 30], // size of the icon
+        popupAnchor: [0, -15], // point from which the popup should open relative to the iconAnchor
+      });
+      L.marker([latitude, longitude], {
+        icon: currentUserIcon,
+      })
+        .addTo(map)
+        .bindPopup("Your location")
+        .openPopup();
     },
   },
 };
