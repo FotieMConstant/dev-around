040c97c8b9d4ae726a5cd4b87de09ae51ff2caa3
FotieMConstant <fotiemoghommahieconstant@gmail.com>
2021-07-16 19:11:53 +0100

    Done working on the api to fetch token from github, takes as unput code gotten from the authorize(Front-end)
    Added the api to the front-end so the app can get the gh_access_token and then login the user and return the user object

--- eru/src/store/index.js
+++ eru/src/store/index.js
@@ -7,11 +7,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    currentUser: "null",
+    currentUser: null,
+    access_token: null,
     darkModeOn: null,
   },
   plugins: [createPersistedState()],
   mutations: {
+    setAccess_token(state, payload){
+      state.access_token = payload;
+    },
+    setCurrentUser(state, payload){
+      state.currentUser = payload;
+    }
   },
   actions: {
   },
